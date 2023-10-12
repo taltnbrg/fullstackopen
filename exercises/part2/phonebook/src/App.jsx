77a2ae27735cd3368b9351881819969f89b97cd9
Tim Altenburg <t.altenburg@interface-medien.de>
2023-10-12 18:25:40 +0200

    2.9 add search functionality

--- exercises/part2/phonebook/src/App.jsx
+++ exercises/part2/phonebook/src/App.jsx
@@ -3,10 +3,15 @@ import Numbers from './components/Numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [searchTerm, setSearchTerm] = useState('')
 
   const handleNewName = (event) => {
     setNewName(event.target.value)
@@ -14,6 +19,17 @@ const App = () => {
 
   const handlenewNumber = (event) => {
     setNewNumber(event.target.value)
+  }
+
+  const handleSearchTerm = (event) => {
+    setSearchTerm(event.target.value)
+  }
+
+  const filterPersonsByName = () => {
+    if( searchTerm.length > 0 ) {
+      return persons.filter( (person) => person.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    }
+    return persons
   }
 
   const handleFormSubmit = (event) => {
@@ -25,9 +41,12 @@ const App = () => {
     setPersons(persons.concat({name: newName, number:newNumber}))
   }
 
+  const showPersons = filterPersonsByName()
+
   return (
     <div>
       <h2>Phonebook</h2>
+      search: <input type="text" onChange={handleSearchTerm} />
       <form onSubmit={handleFormSubmit}>
         <div>
           name: <input type="text" onChange={handleNewName} />
@@ -39,7 +58,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <Numbers persons={persons} />
+      {persons.length > 0 ? <Numbers persons={showPersons} /> : <p>no numbers submitted yet.</p>}
     </div>
   )
 }
