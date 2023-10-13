377aebd90d5c62396d9ef1569826adb496efa84a
Tim Altenburg <t.altenburg@interface-medien.de>
2023-10-13 19:15:58 +0200

    2.12 & 2.13 save phonebook entries to db and extract backend code to service

--- exercises/part2/phonebook/src/App.jsx
+++ exercises/part2/phonebook/src/App.jsx
@@ -1,25 +1,19 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
 import Numbers from './components/Numbers'
 import SearchField from './components/SearchField'
 import PersonInputForm from './components/PersonInputForm'
+import personsService from './services/persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    // { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    // { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    // { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    // { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchTerm, setSearchTerm] = useState('')
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons')
-    .then(response => {
-        setPersons(response.data)
+    personsService.getAll()
+    .then(personsResponse => {
+        setPersons(personsResponse)
     })
   }, [])
 
@@ -42,13 +36,22 @@ const App = () => {
     return persons
   }
 
+  const addPerson = () => {
+    const personObject = {name: newName, number: newNumber}
+    personsService
+      .create(personObject)
+      .then(personResponse => {
+        setPersons(persons.concat(personResponse))
+      })
+  }
+
   const handleFormSubmit = (event) => {
     event.preventDefault()
     if(persons.map(person=>person.name).includes(newName)) {
       alert( `${newName} was already added to the phonebook.` )
       return
     }
-    setPersons(persons.concat({name: newName, number:newNumber}))
+    addPerson()
   }
 
   const showPersons = filterPersonsByName()
